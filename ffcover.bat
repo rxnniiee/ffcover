5d08103cae92d1020c87369c502e657db410e47c
Ronald <23708969+rxnniiee@users.noreply.github.com>
2019-10-26 23:23:33 +0300

    remove windows specific ".exe" call

--- ffcover.bat
+++ ffcover.bat
@@ -5,4 +5,4 @@ ECHO[
 SET /P inputAudio= "Input MP3: "
 SET /P inputImage= "Input image: "
 SET /P outputAudio= "Output MP3: "
-ffmpeg.exe -i %inputAudio% -i %inputImage% -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" %outputAudio%
+ffmpeg -i %inputAudio% -i %inputImage% -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" %outputAudio%
